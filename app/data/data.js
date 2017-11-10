029aac3ab3fce0e7bbca712edd5351e065a5203a
Ben Lambillotte <ben.lambillotte@gmail.com>
2017-11-09 23:47:54 -0800

    Added logic for finding the closest match

---
+++ app/data/data.js
@@ -0,0 +1,17 @@
+
+const puppies = [
+  {
+    fname: 'Sally',
+    puppypic: 'https://gfp-2a3tnpzj.stackpathdns.com/wp-content/uploads/2016/07/Labradoodle-Puppies-For-Sale-600x600.jpg',
+    fluffy: '3',
+    energy: '2'
+  },
+  {
+    fname: 'Trixie',
+    puppypic: 'https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Cesars-Today-Top-Ten-Puppy-Tips.jpg?itok=T2AuVJHq',
+    fluffy: '1',
+    energy: '5'
+  }
+];
+
+module.exports = puppies;
--- app/routing/html_routes.js
+++ app/routing/html_routes.js
@@ -2,6 +2,9 @@
 // =============================================================
 const express = require('express');
 const path = require('path');
+const puppies = require('../data/data');
+
+console.log(puppies);
 
 
 module.exports = (function() {
@@ -21,10 +24,62 @@ module.exports = (function() {
       res.sendFile(__dirname, '../public/static/style.css');
     });
 
+    router.get('/surveys/result/:id', function(req, res) {
+      const id = req.params.id;
+      res.send(JSON.stringify(puppies[id]));
+    });
+
     router.post('/surveys', function(req, res) {
       console.log(req.body);
-      res.end(JSON.stringify(res.body));
+      console.log(puppies);
+
+      const matchIndex = comparePuppies(puppies, req.body);
+      const closestMatch = puppies[matchIndex];
+      console.log(`Closest Match is: ${JSON.stringify(closestMatch)}`);
+      puppies.push(req.body);
+
+      //res.end(JSON.stringify(res.body));
+      res.redirect('/surveys/result/' + matchIndex);
     });
 
     return router;
 })();
+
+
+
+
+function findLeastDiffIndex(valCompare) {
+
+  valCompare.sort(function(a, b) {
+    return a.totalDiff - b.totalDiff;
+  });
+
+  console.log(`Closest Match Index: ${valCompare[0].index}`);
+  return valCompare[0].index;
+}
+
+
+function CompareConstructor(index, diff) {
+  this.index = index;
+  this.totalDiff = diff;
+}
+
+function comparePuppies(puppyArr, newPup) {
+  let valCompare = [];
+
+  for (var i = 0; i < puppyArr.length; i++) {
+    let fluffDiff = Math.abs(parseInt(puppyArr[i].fluffy) - parseInt(newPup.fluffy));
+    let energyDiff = Math.abs(parseInt(puppyArr[i].energy) - parseInt(newPup.energy));
+    let totalDiff = fluffDiff + energyDiff;
+    valCompare.push(new CompareConstructor(i, totalDiff));
+  }
+
+  console.log(valCompare);
+  return findLeastDiffIndex(valCompare);
+}
+
+const test = {
+    fname: 'Test',
+    puppypic: 'ws;dlfkjsd;fs',
+    fluffy: '3',
+    energy: '5' }
